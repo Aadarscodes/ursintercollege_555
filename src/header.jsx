336aa484f1b7435b42278f5845fde11a875a073a
Aadarsh_Chauhan <aadarsh4881@gmail.com>
2024-05-29 00:59:54 +0530

    Aadarsh

--- src/header.jsx
+++ src/header.jsx
@@ -7,28 +7,24 @@ function Header() {
 
   return (
     <div className="Head">
-      <img src={urslogo} className="urslogo" alt="Urs Logo" />
-      
-      <div
-        className="col-md-6 col-sm-8 col-xs-8 logonamesection"
-        style={{ verticalAlign: "middle" }}
-      >
-        <h3 className="logoname logoenglishname">
-          Uma Ram Shankar Singh Inter College
+      <img
+        src={urslogo}
+        className="urslogo"
+        alt="Urs Logo"
+        style={{ verticalAlign: "left" }}
+      />
+      <div>
+        <h1>Uma Ram Shankar Singh Inter College</h1>
+        <h3 style={{ color: "#b75906" }}>
+          <>उमा राम शंकर सिंह इंटर कॉलेज</>
         </h3>
-        <h4 className="logoname logohindiname" style={{ color: "#b75906" }}>
-          <strong>उमा राम शंकर सिंह इंटर कॉलेज</strong>
-        </h4>
-        <h5 className="logoname logohindiname" style={{ color: "#b75906" }}>
-          <strong>मोगलहा, कुनवार, कैम्पियरगंज-गोरखपुर मो. 9140335773</strong>
+        <h5 style={{ color: "#b75906" }}>
+          <>मोगलहा, कुनवार, कैम्पियरगंज-गोरखपुर मो. 9140335773</>
         </h5>
-        <div
-          className="logosubtitle"
-          style={{ color: "#b75906", fontSize: "15px" }}
-        ></div>
-        <div className="logosubtitle logotagname" style={{ color: "#000" }}>
+        <h4 style={{ color: "#b75906", fontSize: "15px" }}></h4>
+        <h5 style={{ color: "#000" }}>
           (Affiliated By: Uttar Pradesh Madhyamik Shiksha Parisad Prayagraj)
-        </div>
+        </h5>
       </div>
     </div>
   );
