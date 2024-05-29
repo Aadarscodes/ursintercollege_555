32d238876570406c542f8b6b262338747061bda7
Abhay <robertsteeve789@gmail.com>
2024-05-29 23:29:07 +0530

    Footer #1
    Signed-off-by: Abhay <robertsteeve789@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/header';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <div>
        <Header/>
        <Navbar></Navbar>
+       <Footer></Footer>
     </div>
   )
 }
